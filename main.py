a9769b51c14756c7077234f486c22d500e02e1ea
AnisimovK <anisimovk89@gmail.com>
2022-10-05 22:12:58 +0500

    Вынес ввод и вывод из функции обработки данных

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
 import requests
 
 
-def weather_check():
+def user_input_place_id():
     place_id = int(input("Для получения информации о погоде введите:"
                          "\n 0 - Череповец; "
                          "\n 1 - Аэропорт Шереметьево; \n 2 - Лондон;\n"))
+    return place_id
+
+
+def weather_check():
     place = ['Череповец', 'SVO', 'London']
     payload = {'MmnqT': '', 'lang': 'ru'}
-    url = f'http://wttr.in/{place[(place_id)]}'
+    url = f'http://wttr.in/{place[(user_input_place_id())]}'
     response = requests.get(url, params=payload)
     response.raise_for_status()
-    print(response.text)
+    return response.text
 
 
 if __name__ == '__main__':
-    weather_check()
+    print(weather_check())
 
 
