4e3630abef78e8254d6eaba53b79244fc21ef586
AnisimovK <anisimovk89@gmail.com>
2022-10-07 12:12:09 +0500

    Put away user input.

--- main.py
+++ main.py
@@ -1,23 +1,20 @@
 import requests
 
 
-def user_input_place_id():
-    place_id = int(input("Для получения информации о погоде введите:"
-                         "\n 0 - Череповец; "
-                         "\n 1 - Аэропорт Шереметьево; \n 2 - Лондон;\n"))
-    return place_id
+def weather_request(place):
 
-
-def weather_check():
-    place = ['Череповец', 'SVO', 'London']
     payload = {'MmnqT': '', 'lang': 'ru'}
-    url = f'http://wttr.in/{place[(user_input_place_id())]}'
+    url = f'http://wttr.in/{place}'
     response = requests.get(url, params=payload)
     response.raise_for_status()
     return response.text
 
 
+def weather_print():
+    places = ['Череповец', 'SVO', 'London']
+    for place in places:
+        print(weather_request(place))
+
+
 if __name__ == '__main__':
-    print(weather_check())
-
-
+    weather_print()
